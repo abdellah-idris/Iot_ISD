642ab60f66ec13f44190a2102fa36fb1e7ba8e3a
IDRIS ABDELLAH <50142372+abdellah-idris@users.noreply.github.com>
2023-04-04 13:56:27 +0000

    publisher

--- reception_tir.py
+++
@@ -1,8 +0,0 @@
-import Infralib
-import RPi.GPIO as GPIO
-
-IR_RECEIVER = 15
-
-GPIO.setup(IR_RECEIVER, GPIO.IN)
-
-GPIO.add_event_detect(IR_RECEIVER, GPIO.FAILLING, callback=lambda x : InfraLib.getSignal(IR_RECEIVER, client), bouncetime=100)
