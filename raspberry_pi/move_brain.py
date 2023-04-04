a4884201f557faf4c43ee6c30defaf46570e1643
IDRIS ABDELLAH <idrisabdellah@outlook.fr>
2023-04-04 23:04:39 +0200

    refactor: delete unused files

--- raspberry_pi/move_brain.py
+++
@@ -1,8 +0,0 @@
-import Adafruit_PCA9685 # Import the library used to communicate with PCA9685  
-
-pwm = Adafruit_PCA9685.PCA9685()    # Instantiate the object used to control the PWM  
-
-pwm.set_pwm_freq(50)    # Set the frequency of the PWM signal  
-   
-def move_brain(channel, angle):
-    pwm.set_pwm(channel, 0, angle)
